bd0c03b66c4b46e3b1aab1b19e0f8eb1c7f0cac2
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-02-24 16:01:31 +1100

    added levelID

--- New Unity Project/Assets/Scripts/Touch/ContestManager.cs
+++ New Unity Project/Assets/Scripts/Touch/ContestManager.cs
@@ -18,13 +18,108 @@ public class ContestManager : MonoBehaviour
     {
         if ( first != 0 && second != 0)
         {
-            if (second + 1 == first || second - 2 == first)
+            if (first == second)
             {
-                Debug.Log("second wins");
+                Debug.Log("Draw");
             }
-            else
+            if (first == 1)
             {
-                Debug.Log("first wins");
+                if (second <= 8)
+                            {
+                    Debug.Log("second wins");
+                }
+                else
+                {
+                    Debug.Log("first wins");
+                }
+            }
+            if (first == 2)
+            {
+                if (second <= 9 && second >= 2)
+                {
+                    Debug.Log("second wins");
+                }
+                else
+                {
+                    Debug.Log("first wins");
+                }
+            }
+            if (first == 3)
+            {
+                if (second <= 10 && second >= 3)
+                {
+                    Debug.Log("second wins");
+                }
+                else
+                {
+                    Debug.Log("first wins");
+                }
+            }
+            if (first == 4)
+            {
+                if (second <= 11 && second >= 4)
+                {
+                    Debug.Log("second wins");
+                }
+                else
+                {
+                    Debug.Log("first wins");
+                }
+            }
+            if (first == 5)
+            {
+                if (second <= 12 && second >= 5)
+                {
+                    Debug.Log("second wins");
+                }
+                else
+                {
+                    Debug.Log("first wins");
+                }
+            }
+            if (first == 6)
+            {
+                if (second <= 13 && second >= 6)
+                {
+                    Debug.Log("second wins");
+                }
+                else
+                {
+                    Debug.Log("first wins");
+                }
+            }
+            if (first == 7)
+            {
+                if (second <= 14 && second >= 7)
+                {
+                    Debug.Log("second wins");
+                }
+                else
+                {
+                    Debug.Log("first wins");
+                }
+            }
+            if (first == 8)
+            {
+                if (second <= 15 && second >= 8)
+                {
+                    Debug.Log("second wins");
+                }
+                else
+                {
+                    Debug.Log("first wins");
+                }
+            }
+            if (first == 9)
+            {
+                if (second >= 9 && second >= 9)
+                {
+                    Debug.Log("second wins");
+                }
+                else
+                {
+                    Debug.Log("first wins");
+                }
             }
             Debug.Log(first);
             Debug.Log(second);
